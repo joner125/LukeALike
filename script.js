ef7d0cb52fe0549e05c2b23bf9e14535230f4b63
joner125 <joner125@hotmail.com>
2019-11-30 19:26:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
-  });
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
 });
